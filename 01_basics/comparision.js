52741b912e151678e48c9299b18fa58a8377f95f
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-09-07 16:03:17 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,14 @@
+// these are just normal comparisions 
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 >= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// The following are some tricky comparisions 
+
+console.log("2" > 1) //auto matically string 2 converted into number (expected answer would be true cuz 2 > 1)
+console.log("1" == 1) //expected anwer is true
+
+console.log("1" === 1) //comparing two types including datatype(===) => expected answer is false
