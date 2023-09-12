dbb54862c3812e46108ec77427ac2ddac1287589
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-09-12 08:24:38 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+//defining a function
+
+function sayMyName(){
+    console.log("your name is Raju")
+}
+
+// sayMyName() //calling a function
+
+function returnSomething(){
+    return "hi there my name is v s raju" //returns whatever we defined
+}
+
+// console.log(returnSomething()) //return and console are not same
+
+//function with parameters
+function adding(num1,num2){
+    return num1+num2
+}
+
+console.log(adding(2,3)) //passing arguments
