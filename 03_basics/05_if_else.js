d292f1c71ba148f449d29a80246978c8ad64e7a4
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-09-13 15:44:15 +0000

    if else

---
+++ 03_basics/05_if_else.js
@@ -0,0 +1,10 @@
+//control flow is very import and mostly used in every js program
+//if else
+
+const name = "raju"
+
+if (name == "raju"){
+    console.log("your name is raju")
+} else{
+    console.log("you are something else")
+}
