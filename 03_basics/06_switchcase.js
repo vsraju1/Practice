0a9e7e3374277f1cc31121a35b8be974bc31823d
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-09-13 15:53:28 +0000

    switch case

---
+++ 03_basics/06_switchcase.js
@@ -0,0 +1,29 @@
+//we cannot always use if else, else if
+//in such situations we can use switch case
+
+const name = "kamal"
+
+// syntax
+
+// switch(key){
+//     case "string"| num :
+//            console.log("skfjosdfi");
+//            break;
+// //  
+
+//     default:
+//     break
+// }
+
+switch(name){
+    case "raju": 
+    console.log("you are not kamal");
+    break;
+
+    case "kamal":
+        console.log("hi kamal");
+        break;
+
+    default:
+        break;
+}
