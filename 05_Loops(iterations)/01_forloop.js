995ddd4c74b0285466f961faea9a5d1068c9ac02
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-09-14 06:42:49 +0000

    for loop

---
+++ 05_Loops(iterations)/01_forloop.js
@@ -0,0 +1,21 @@
+for (let i = 1; i <= 10; i++) {
+    // const element = i;
+    // console.log(`${i} table`)
+    for (let j = 1; j <= 10; j++) {
+        // const element = j;
+        // console.log(`${i} * ${j} = ${i*j}`)
+        
+    }
+    
+}
+
+//break
+
+for (let i = 0; i < 10; i++) {
+    if (i == 5){
+        console.log("detects 5");
+        break;
+    }
+    console.log(i)
+    
+}
