842f66bd8ea0205948b66328cbfcd7f36fc15b6a
vsraju1 <69906555+vsraju1@users.noreply.github.com>
2023-09-15 15:10:13 +0000

    async basics

---
+++ 06_asynchronous/01_async_basics.js
@@ -0,0 +1,7 @@
+//javascript is synchronous and sigle threaded language
+
+//execution context
+//it will execute line by line
+//each operation waits for the last one to complete before executing
+
+//timeout and timeinterval are asunchronous in javascript
